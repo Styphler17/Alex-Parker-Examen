716bab7d1ff916fe935df0c41b61a61073aa17bc
Stiffler-A <styphler17@gmail.com>
2025-10-06 02:54:18 +0200

    Mise en place des fichiers public : index.php .htaccess

--- public/index.php
+++ public/index.php
@@ -1,17 +1,5 @@
 <?php
 
-// Initialisation des zones dynamiques
-$content = '';
-$title = '';
-
-// Base URL
-define('BASE_URL', '/ALEX-PARKER-EXAMEN/public/');
-
-// Assets URL
-define('ASSETS_URL', '/ALEX-PARKER-EXAMEN/public/');
-
-// Paramètres de connexion
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'alex_parker_octobre_2021');
-define('DB_USER', 'root');
-define('DB_PWD', '');
+require_once '../core/init.php';
+require_once '../app/routers/index.php';
+require_once '../app/views/template/layout.php';
