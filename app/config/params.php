76b081892c70ae662519780a90cf1ae5299e42b4
Stiffler-A <styphler17@gmail.com>
2025-10-06 02:48:35 +0200

    Création du params.php

--- app/config/params.php
+++ app/config/params.php
@@ -1,3 +1,17 @@
 <?php
-// params.php - placeholder
-return [];
+
+// Initialisation des zones dynamiques
+$content = '';
+$title = '';
+
+// Base URL
+define('BASE_URL', '/Alex-Parker-Examen/public/');
+
+// Assets URL
+define('ASSETS_URL', '/Alex-Parker-Examen/public/');
+
+// Paramètres de connexion
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'alex_parker_octobre_2021');
+define('DB_USER', 'root');
+define('DB_PWD', '');
